1f3324e2ac814aa9545ffc4fe88da07dc7528569
Eric Branson <eric.branson@gmail.com>
2024-11-19 13:11:31 -0700

    fixed contentPath

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import aspectRatio from '@tailwindcss/aspect-ratio';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/skeleton/plugin'
+import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin'
 import * as themes from '@skeletonlabs/skeleton/themes'
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}',
+    contentPath(import.meta.url, 'svelte'),
+  ],
 
   theme: {
     extend: {}
